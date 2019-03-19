bde6be1cce3d9ff0979d8b068a1e07c98a24e39a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-18 21:53:28 -0400

    Add route to add posts

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -8,7 +8,16 @@ router.get('/', async (req, res) => {
   const posts = await loadPostsCollection();
   res.send(await posts.find({}).toArray());
 });
+
 // Add Post
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+  res.status(201).send();
+});
 
 // Delete Post
 
