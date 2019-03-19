c992c591d46ca63eec97a8abce442cf4be908d4c
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-18 21:49:24 -0400

    Add get post route

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,11 +4,22 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // Get Posts
-router.get('/', (req, res) => {
-  res.send('hello');
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
 });
 // Add Post
 
 // Delete Post
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    'mongodb://amsalud:password123@ds117866.mlab.com:17866/postboard',
+    {
+      useNewUrlParser: true
+    }
+  );
+  return client.db('postboard').collection('posts');
+}
 
 module.exports = router;
